e3903ea172fa8e9a0337432c283d4d17018ff342
maselof <mr.gindeev@gmail.com>
2023-10-20 16:30:51 +0300

    add_goroutines_and_channels

--- api/controller.go
+++ api/controller.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	models "wb_task_l0/database"
 	"wb_task_l0/streaming"
 
 	"github.com/nats-io/stan.go"
@@ -21,18 +22,29 @@ func StartPage(w http.ResponseWriter, r *http.Request) {
 
 // Make a new connection to the STAN server, create a new subscriber and send the data to the Web
 func sendDataHandler(w http.ResponseWriter, r *http.Request) {
+	orderId := r.FormValue("orderId")
+	var resChan chan models.Orders = make(chan models.Orders)
+
+	go connectAndCreateSubscriber(orderId, resChan)
+	order, opened := <-resChan
+	if !opened {
+		log.Fatal("Channel is closed")
+	}
+
+	tml, err := template.ParseFiles("templates/main.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tml.Execute(w, order)
+}
+
+func connectAndCreateSubscriber(orderId string, ch chan models.Orders) {
 	clientName := fmt.Sprintf("client-%d", rand.Int())
 	sc, err := stan.Connect("mystreamingserver", clientName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	sub := streaming.NewSubscriber(&sc, clientName)
-	orderId := r.FormValue("orderId")
-
 	order := sub.Subscribe(orderId)
-	tml, err := template.ParseFiles("templates/main.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	tml.Execute(w, order)
+	ch <- order
 }
